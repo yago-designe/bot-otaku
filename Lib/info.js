4a56d733786bbd292d70cc2b64028030cecc032c
Comunidade.de.otakus <76886425+yago-designe@users.noreply.github.com>
2021-01-03 03:47:53 -0300

    Add files via upload

---
+++ Lib/info.js
@@ -0,0 +1,44 @@
+exports.info = (id, A187, tampilTanggal, whatsapp, youtube, tampilWaktu, instagram, nomer, aktif) => {
+	return ` 
+❉──────────❉
+
+*${A187}*
+
+❉──────────❉
+
+📆 *${tampilTanggal}*
+📌STATUS BOT ATIVO: *${aktif}*
+
+         
+╔════════════════════
+║ *Feito por: $COMUNIDADE OTAKU*
+╠════════════════════
+║   *⚠️ATENÇÃO⚠️*
+╠════════════════════
+║*SOMOS UMA PAGINA DEDICADA A SUA*
+║*DIVERSÃO*
+╠════════════════════
+║╭──❉ *INFO* ❉──
+║│➸ *AUTOR*: Yago
+║│➸ *DESIGNER*: Yago
+║│➸ *ADM*:  Yagoツ
+║╰───────────
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│➸ *Faça login aqui😙*
+║│ _${wa.me/5521998250174}_
+║│➸ *admin*
+║│ _${yago}_
+║│➸ *Instagram <Segue>*
+║│ _${@comunidade.de.otakus}_
+║│➸ *Criador*
+║│    Yago ツ
+║╰───────────
+║         ${A187}
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║         ${A187}
+╠════════════════════
+║ _*Feito por Yagoツ*_
+╚════════════════════`
+}
---
+++ Lib/menu1.js
@@ -0,0 +1,57 @@
+exports.menu1 = (id, A187, tampilTanggal, whatsapp, youtube, tampilWaktu, instagram, nomer, aktif) => {
+	return ` 
+❉──────────❉
+
+COMUNIDADE OTAKU 🤨
+
+❉──────────❉
+
+COMUNIDADE OTAKU🤠🤙 OLHE OS COMANDOS ABAIXO
+
+╔════════════════════
+║ 
+╠════════════════════
+║╭──❉ *MEDIAS* ❉──
+║│➸ _*!sticker*_
+║│➸ _*!foto menina*_
+║│➸ _*!foto cara*_
+║│➸ _*!pokemon*_ 
+║│➸ _*!loli*_
+║│➸ _*!fotoanime*_
+║│➸ _*!waifu*_
+║│➸ _*!ttp*_ <texto>
+║│➸ _*!meme*_
+║│➸ _*!ssweb*_ <link>
+║╰───────────
+╠════════════════════
+║╭──❉ *EDUCAÇÃO* ❉──
+║│➸ _*!nulis*_ <área>
+║│➸ _*!pantun*_
+║│➸ _*!artinama*_ <seu nome>
+║│➸ _*!pasangan*_ <Aris & Cahya>
+║│➸ _*!lirik*_ <nome da musica>
+║│➸ _*!chord*_ <nome da musica>
+║│➸ _*!bucin*_
+║│➸ _*!zodiak*_ <28-02-2002>
+║│➸ _*#*_  <teks>
+║│➸ _*!cooltext*_ <texto>
+║╰───────────
+╠════════════════════
+║╭──❉ *INFORMAÇÕES* ❉──
+║│➸ _*!sholat*_ <área>
+║│➸ _*!covidID*_
+║│➸ _*!covidcountry*_ <país>
+║│➸ _*!infogempa*_ 
+║│➸ _*!cektanggal*_ <encontro>
+║│➸ _*!infoanime*_ <Naruto>
+║│➸ _*!filmanime*_ <Naruto>
+║│➸ _*!wiki*_ <crocodilo>
+║│➸ _*!wikien*_ <dog>
+║│➸ _*!resep*_ <arroz>
+║│➸ _*!jadwalTVnow*_ <canal>
+║╰───────────
+
+╠════════════════════
+║ COMUNIDADE OTAKU 🤠🤙
+╚════════════════════`
+}
